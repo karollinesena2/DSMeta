3f280e826bee2c5150e87bc4735513d7c6c77fdc
Anna <karollinesena@hotmail.com>
2022-11-14 22:27:16 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import NotificationButton from "./Components/NotificationButton"
+
+import Header from "./Components/Header"
+import SalesCard from "./Components/SalesCard"
 
 function App() {
 return (
   <>
-  <h1>Hello world</h1>
-  <NotificationButton></NotificationButton>
+  <Header></Header>
+  <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard></SalesCard>
+        </div>
+          </section>
+          </main>
   </>
 )
 }
 
-export default App
+export default App 
